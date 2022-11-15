c21fccc8c2082fb65f88a3b5196764e743b18041
Ali Yasin Emir <aliyasin.emir16@gmail.com>
2022-11-15 19:56:27 +0300

    the project finished

--- app.js
+++ app.js
@@ -2,7 +2,17 @@
 const modalBtn = document.querySelector('.modal-btn');
 const overlay = document.querySelector('.modal-overlay');
 const closeBtn = document.querySelector('.close-btn');
+const main = document.querySelector('.main');
+
 // listen for click events on modal-btn and close-btn
-modalBtn.addEventListener('click');
+modalBtn.addEventListener('click', function () {
+  // console.log(modalBtn.classList);
+  overlay.classList.add('open-modal');
+});
+
+closeBtn.addEventListener('click', function () {
+  overlay.classList.remove('open-modal');
+});
+
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
